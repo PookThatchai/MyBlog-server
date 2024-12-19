28505000a6f23a1fa772c7431c157975468bde2b
Thatchai Taikla <becomedev1998@gmail.com>
2024-12-20 00:23:23 +0700

    updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
-const uploadMiddleware = multer({ dest: "uploads/" }); // ใช้ multer เพื่อจัดการไฟล์ที่อัปโหลด
+const uploadMiddleware = multer({ dest: "uploads/" });
 require("dotenv").config();
 app.use("/uploads", express.static("uploads"));
 
@@ -20,6 +20,11 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+};
+app.use(cors(corsOptions));
 
 const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
@@ -36,7 +41,6 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-app.use(cors());
 app.use(express.json());
 
 mongoose
@@ -54,7 +58,7 @@ app.post("/register", async (req, res) => {
       password: hashedPassword,
     });
 
-    res.json(userDoc);
+    res.json({ user: userDoc, frontendUrl: process.env.FRONTEND_URL });
   } catch (error) {
     res.status(400).json(error);
   }
@@ -86,148 +90,6 @@ app.post("/login", async (req, res) => {
     res.status(400).json("Incorrect information");
   }
 });
-
-app.get("/profile", verifyToken, (req, res) => {
-  const userProfile = {
-    id: req.user.id,
-    username: req.user.username,
-  };
-  res.json(userProfile);
-});
-
-app.post("/logout", (req, res) => {
-  res.json({ message: "Logged out successfully" });
-});
-
-app.post("/createpost", uploadMiddleware.single("file"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "No file uploaded" });
-  }
-
-  cloudinary.uploader.upload(req.file.path, async (error, result) => {
-    if (error) {
-      return res
-        .status(500)
-        .json({ message: "Error uploading image to Cloudinary", error });
-    }
-
-    const imageUrl = result.secure_url;
-
-    const { title, summary, content } = req.body;
-    if (!title || !summary || !content) {
-      return res.status(400).json({ message: "Incomplete data" });
-    }
-
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (!token) {
-      return res.status(403).json({ message: "No token provided" });
-    }
-
-    try {
-      const decoded = jwt.verify(token, secret);
-      const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover: imageUrl,
-        author: decoded.id,
-      });
-      res.json(postDoc);
-    } catch (error) {
-      return res.status(401).json({ message: "Failed to authenticate token" });
-    }
-  });
-});
-
-// ดึงข้อมูลโพสต์
-app.get("/createpost", async (req, res) => {
-  try {
-    const posts = await Post.find()
-      .sort({ createAt: -1 })
-      .populate("author", "username")
-      .limit(20);
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching posts" });
-  }
-});
-
-app.get("/createpost/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const postDoc = await Post.findById(id).populate("author", ["username"]);
-
-    if (!postDoc) {
-      return res.status(404).json({ message: "Post not found" });
-    }
-
-    res.json(postDoc);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching post", error });
-  }
-});
-
-// แก้ไขโพสต์
-app.put(
-  "/createpost/:id",
-  uploadMiddleware.single("file"),
-  async (req, res) => {
-    const { id } = req.params; // ดึง id ของโพสต์จาก URL
-    const { title, summary, content } = req.body; // ดึงข้อมูลโพสต์จาก request body
-    let cover = req.body.cover; // ค่า cover หากไม่มีการอัปโหลดใหม่จะใช้ค่าเดิม
-
-    if (!title || !summary || !content) {
-      return res.status(400).json({ message: "Incomplete data" });
-    }
-
-    if (req.file) {
-      try {
-        const cloudinaryResult = await cloudinary.uploader.upload(
-          req.file.path
-        );
-        cover = cloudinaryResult.secure_url; // อัปเดต URL ของไฟล์ที่อัปโหลดใหม่
-      } catch (error) {
-        return res
-          .status(500)
-          .json({ message: "Error uploading image to Cloudinary", error });
-      }
-    }
-
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (!token) {
-      return res.status(403).json({ message: "No token provided" });
-    }
-
-    try {
-      const decoded = jwt.verify(token, secret);
-
-      const postDoc = await Post.findById(id);
-
-      if (!postDoc) {
-        return res.status(404).json({ message: "Post not found" });
-      }
-
-      if (postDoc.author.toString() !== decoded.id) {
-        return res
-          .status(403)
-          .json({ message: "You are not authorized to edit this post" });
-      }
-      postDoc.title = title;
-      postDoc.summary = summary;
-      postDoc.content = content;
-      postDoc.cover = cover;
-      await postDoc.save();
-
-      res.json({ message: "Post updated successfully", post: postDoc });
-    } catch (error) {
-      console.error(error);
-      return res
-        .status(500)
-        .json({ message: "Error during post update", error });
-    }
-  }
-);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
